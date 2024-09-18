f3fd0f4d7857674d8025b1135a2235595be8fd67
huanhaowu <1104326@est.intec.edu.do>
2024-09-17 22:01:34 -0400

    feat: kanban board

--- src/components/modal.svelte
+++ src/components/modal.svelte
@@ -1,22 +1,37 @@
 <div>
 	<div class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 z-50 overflow-auto" on:click={closeByBackgroundClick? close : ''}>
-		<div class="mx-auto my-36 w-[600px] bg-white rounded-md shadow-xl p-10" on:click|stopPropagation>
+<!--		<div class="mx-auto my-36 w-[600px] bg-white rounded-md shadow-xl p-10" style="width: {width}" on:click|stopPropagation>-->
+		<div class="mx-auto my-36 bg-white rounded-md shadow-xl p-10" style="width: {width}" on:click|stopPropagation>
 			{#if header}
 				<div class="header">
 					<div class="title">
-						{title}
+						{#if title}{title}{/if}
 					</div>
 
-					<div class="controls">
-						{#if controlOptions}
-							<button>
-								<Ellipsis />
+					{#if controls}
+						<div class="controls">
+							{#if controlsOptions}
+								<button use:clickOutside
+												on:click_outside={() => { if(userDropdown) userDropdown = !userDropdown }}
+												on:click={() => { userDropdown = !userDropdown}}
+								>
+									<Ellipsis />
+								</button>
+
+								<div class="dropdown-menu-container {userDropdown ? 'open' : ''}">
+									<div class="dropdown-menu">
+										<button on:click={() => {showModal = true}}>
+											Eliminar tarea
+											<Trash2  size={20}/>
+										</button>
+									</div>
+								</div>
+							{/if}
+							<button on:click={close}>
+								<X />
 							</button>
-						{/if}
-						<button on:click={close}>
-							<X />
-						</button>
-					</div>
+						</div>
+					{/if}
 				</div>
 			{/if}
 			<div class="content">
@@ -26,17 +41,32 @@
 	</div>
 </div>
 
+<DeleteTaskModal show={showModal} on:close={handleClose}/>
+
 <script>
 	import {createEventDispatcher} from 'svelte';
-	import { Ellipsis, X } from 'lucide-svelte';
+	import { Ellipsis, Trash2, X } from 'lucide-svelte';
+	import {clickOutside} from '$lib/clickOutside.js';
+	import DeleteTaskModal from '$components/modals/deleteTask.modal.svelte';
 
 	const dispatch = createEventDispatcher();
 
+	export let width = '600px'
 	export let show = true;
 	export let header = false
 	export let title
-	export let controlOptions = false
+	export let controls = false
+	export let controlsOptions = false
 	export let closeByBackgroundClick = false
+
+
+	// Delete Task Modal
+	let showModal = false
+	let userDropdown = false
+
+	function handleClose(){
+		showModal = false
+	}
 
 	const close = () => {
 		show = false;
@@ -48,9 +78,43 @@
     .header{
         display: flex;
         justify-content: space-between;
+        position: relative;
     }
 
 		.header .title{
 				font-size: var(--luma-title-font-size);
 		}
+
+    .dropdown-menu-container{
+        position: absolute;
+        top: 50%;
+        right: 1rem;
+        width: 220px;
+        max-height: 0px;
+        overflow: hidden;
+        z-index: 9999;
+    }
+    .dropdown-menu-container.open{
+        max-height: 400px;
+    }
+
+    .dropdown-menu{
+        overflow: hidden;
+        border-radius: 5px;
+        background-color: var(--luma-color-gray-50);
+        padding: 20px;
+        margin: 10px;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    }
+
+		.dropdown-menu button{
+				width: 100%;
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+		}
+
+    .dropdown-menu button:hover{
+				color: var(--luma-color-red-error);
+    }
 </style>
