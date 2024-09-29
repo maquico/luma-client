173091be3b1de9b27c5432a68bbc9a082b3d34fe
GleidyEspinal <1104225@est.intec.edu.do>
2024-09-28 23:27:18 -0400

    feat: general shop ui

--- src/routes/(app)/shop/general/+page.svelte
+++ src/routes/(app)/shop/general/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { ImageIcon, Info } from 'lucide-svelte'; // Asegúrate de importar los íconos que necesites
 	let themes = [
 		'light',
 		'dark',
@@ -39,29 +40,40 @@
 	}
 </script>
 
-<h1>Shop UI</h1>
-<div class="themes">
-	{#each themes as theme}
-		<button
-			class="btn btn-secondary"
-			on:click={() => {
-				setTheme(theme);
-			}}
-		>
-			{theme}
-		</button>
-	{/each}
-</div>
+<div class="h-screen w-full overflow-y-auto p-4 bg-white">
+	<div class="grid grid-cols-4 gap-[var(--luma-element-spacing)]">
+		{#each themes as theme}
+			<div class="bg-white rounded-lg custom-shadow overflow-hidden flex flex-col justify-between">
+				<div class="flex justify-between items-center p-2 bg-white text-gray-700">
+					<span>{theme}</span>
+					<button class="text-gray-500 hover:text-gray-700">
+						<Info class="w-5 h-5" />
+					</button>
+				</div>
 
-<div class="p-5">
-	<a href="/" class="btn btn-primary"> Home </a>
+				<div class="flex justify-center items-center h-24 bg-gray-300">
+					<ImageIcon class="w-12 h-12 text-gray-400" />
+				</div>
+
+				<div class="p-2">
+					<button
+						class="w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-1 rounded-md"
+						on:click={() => setTheme(theme)}
+					>
+						Seleccionar
+					</button>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
-	.themes {
-		flex-wrap: wrap;
-		margin: 2rem;
-		display: flex;
-		gap: 1rem;
+	.custom-shadow {
+		box-shadow:
+			0 2px 4px rgba(0, 0, 0, 0.05),
+			0 -2px 4px rgba(0, 0, 0, 0.05),
+			2px 0 4px rgba(0, 0, 0, 0.05),
+			-2px 0 4px rgba(0, 0, 0, 0.05);
 	}
 </style>
