71cf23e33b20fd9b83d0e41ef6abe5db9f59a7e5
GleidyEspinal <1104225@est.intec.edu.do>
2024-09-30 21:53:03 -0400

    feat: create task integration in progress

--- src/components/modals/createTask.modal.svelte
+++ src/components/modals/createTask.modal.svelte
@@ -1,74 +1,79 @@
 <script>
-	import Modal from '$components/modal.svelte'
-	import {createEventDispatcher} from 'svelte';
+	import Modal from '$components/modal.svelte';
+	import { createEventDispatcher } from 'svelte';
 	import axios from 'axios';
 	import { goto } from '$app/navigation';
 	import { Tags, User } from 'lucide-svelte';
 	import { DateInput, DatePicker } from 'date-picker-svelte';
-	let date = new Date()
-	let showDatePicker = false
-
+	let date = new Date();
+	let showDatePicker = false;
 
 	const dispatch = createEventDispatcher();
 
 	export let show = true;
-	export let data
+	export let data;
 
 	const close = () => {
 		show = false;
-		dispatch('close')
-	}
+		formData = { ...initialFormData };
+		dispatch('close');
+	};
 
+	const initialFormData = {
+		projectId: 1,
+		name: '',
+		priority: '',
+		time: '',
+		startDate: '',
+		endDate: '',
+		userId: '37d3b652-d314-4124-9685-add5f0c6fc19',
+		description: '',
+		tags: ''
+	};
+
+	let formData = { ...initialFormData };
 
 	function validate() {
-		console.log("I'm the validate() function")
-		// TODO: validate password with db data
-		// if (email.match(validRegex) && password){
-		//
-		// 	axios.post('http://localhost:3000/api/session', {
-		// 		email: email,
-		// 		password: password,
-		// 	})
-		// 		.then(response => {
-		// 			console.log(response.data);
-		// 			console.log('successfull login');
-		// 			goto('/');
-		// 		})
-		// 		.catch(error => {
-		// 			console.error('Error:', error);
-		// 		});
-		//
-		// 	invalidInput=false
-		// }else{
-		// 	console.log('error login');
-		// 	invalidInput=true
-		// }
-	}
+		console.log('Validating form data...');
 
+		try {
+			const response = axios.post('https://luma-server.onrender.com/api/task', {
+				projectId: formData.projectId,
+				name: formData.name,
+				priority: formData.priority,
+				time: formData.time,
+				description: formData.description
+			});
+
+			console.log('Task created successfully:', response.data);
+
+			close();
+		} catch (error) {
+			console.error('Error creating task:', error);
+		}
+	}
 </script>
 
 {#if show}
-	<Modal
-		header
-		controls
-		controlsOptions
-		on:close={close}
-	>
+	<Modal header controls controlsOptions on:close={close}>
 		<form on:submit|preventDefault={validate}>
 			<div class="overview">
-				<p class="project-name"> Project name </p>
-				<p class="task-name"> Task name </p>
+				<p class="project-name">Project name</p>
 			</div>
+
+			<label class="input input-bordered flex items-center gap-2">
+				<input type="text" bind:value={formData.name} class="grow" placeholder="Task name" />
+			</label>
 
 			<div class="task-details">
 				<label class="input input-bordered flex items-center gap-2">
 					<User />
-					<input type="text" class="grow" placeholder="Search" />
+					<input type="text" class="grow" placeholder="User" />
 				</label>
 
 				<label class="input input-bordered flex items-center gap-2">
 					<Tags />
-					<input type="text" class="grow" placeholder="Search" />
+					<input type="text" bind:value={formData.tags} class="grow" placeholder="Tags" />
 				</label>
 			</div>
 
@@ -76,45 +81,64 @@
 				<div class="row">
 					<label class="form-control w-full max-w-xs">
 						<div class="label">
-							<span class="label-text">Pick the best fantasy franchise</span>
+							<span class="label-text">Esfuerzo</span>
 						</div>
-						<select class="select select-bordered">
-							<option disabled selected>Pick one</option>
-							<option>Star Wars</option>
-							<option>Harry Potter</option>
-							<option>Lord of the Rings</option>
-							<option>Planet of the Apes</option>
-							<option>Star Trek</option>
+						<select class="select select-bordered" bind:value={formData.time}>
+							<option disabled selected></option>
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
 						</select>
 					</label>
 
 					<label class="form-control w-full max-w-xs">
 						<div class="label">
-							<span class="label-text">Pick the best fantasy franchise</span>
+							<span class="label-text">Prioridad</span>
 						</div>
-						<DateInput bind:value={date} on:click={() => {showDatePicker = !showDatePicker}}/>
-						{#if showDatePicker}
-							<DatePicker bind:value={date} />
-						{/if}
+						<select class="select select-bordered" bind:value={formData.priority}>
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</select>
 					</label>
 				</div>
 
 				<div class="row">
 					<label class="form-control w-full max-w-xs">
 						<div class="label">
-							<span class="label-text">Pick the best fantasy franchise</span>
+							<span class="label-text">Fecha inicio</span>
 						</div>
-						<select class="select select-bordered">
-							<option disabled selected>Pick one</option>
-							<option>Star Wars</option>
-							<option>Harry Potter</option>
-							<option>Lord of the Rings</option>
-							<option>Planet of the Apes</option>
-							<option>Star Trek</option>
-						</select>
+						<DateInput
+							bind:value={date}
+							on:click={() => {
+								showDatePicker = !showDatePicker;
+							}}
+						/>
+						{#if showDatePicker}
+							<DatePicker bind:value={date} />
+						{/if}
 					</label>
 
 					<label class="form-control w-full max-w-xs">
+						<div class="label">
+							<span class="label-text">Fecha fin</span>
+						</div>
+						<DateInput
+							bind:value={date}
+							on:click={() => {
+								showDatePicker = !showDatePicker;
+							}}
+						/>
+						{#if showDatePicker}
+							<DatePicker bind:value={date} />
+						{/if}
+					</label>
+
+					<!-- <label class="form-control w-full max-w-xs">
 						<div class="label">
 							<span class="label-text">Pick the best fantasy franchise</span>
 						</div>
@@ -126,62 +150,54 @@
 							<option>Planet of the Apes</option>
 							<option>Star Trek</option>
 						</select>
-					</label>
+					</label> -->
 				</div>
 
 				<label class="form-control">
 					<div class="label">
-						<span class="label-text">Your bio</span>
+						<span class="label-text">Descripción</span>
 					</div>
-					<textarea class="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+					<textarea
+						class="textarea textarea-bordered h-24"
+						bind:value={formData.description}
+						placeholder="Descricion de la tarea"
+					></textarea>
 				</label>
-
-				<label class="form-control">
-					<div class="label">
-						<span class="label-text">Your bio</span>
-					</div>
-					<textarea class="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
-				</label>
-
 			</div>
 
 			<div class="controls">
-				<button type="submit" class="btn btn-primary">
-					Guardar
-				</button>
+				<button type="submit" class="btn btn-primary"> Guardar </button>
 			</div>
 		</form>
-<!--		{#if data}-->
-<!--			{data}-->
-<!--		{/if}-->
+		<!--		{#if data}-->
+		<!--			{data}-->
+		<!--		{/if}-->
 	</Modal>
 {/if}
 
 <style>
-    form{
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-    }
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
 
-		.task-details{
-				display: flex;
-				flex-direction: column;
-				gap: 1rem;
-		}
+	.task-details {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
 
-		textarea{
-				resize: none;
-		}
+	textarea {
+		resize: none;
+	}
 
-		.row{
-				display: flex;
-				gap: 1rem;
-		}
+	.row {
+		display: flex;
+		gap: 1rem;
+	}
 
-    .controls{
-        text-align: right;
-    }
-
-
+	.controls {
+		text-align: right;
+	}
 </style>
