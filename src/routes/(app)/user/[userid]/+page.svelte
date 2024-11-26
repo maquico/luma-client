3d818a22ff0da89a1df85e933d6e180c86f288fd
huanhaowu <1104326@est.intec.edu.do>
2024-11-25 22:10:38 -0400

    feat: search bar and minor styles

--- src/routes/(app)/user/[userid]/+page.svelte
+++ src/routes/(app)/user/[userid]/+page.svelte
@@ -3,54 +3,35 @@
 	import { LockKeyhole } from 'lucide-svelte';
 	import axios from 'axios';
 	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
+	import { page } from '\$app/stores';
 	import { goto, invalidate } from '$app/navigation';
 
 	let showModal = false;
-	let selectedBadge;
-	let selectedProfile;
-	let users = [];
-	let userData = [];
-	let userBadges = [];
+	let selectedBadge
+	let selectedProfile
+	let users = []
+	let userData = []
+	let userBadges = []
 	let currentPage = 1;
 	const itemsPerPage = 10; // 2 rows of 5 columns
 	let loading = true;
-	let userId = '37d3b652-d314-4124-9685-add5f0c6fc19';
-	let badges = [];
+	let searchQuery = "";
 
 	onMount(async () => {
-		await getAllUsers();
-		await getUserBadges();
-		await getUserInfo();
+		await getAllUsers()
+		await getUserBadges()
+		await getUserInfo()
+	})
+
+	$: currentUserID = $page.params.userid ? $page.params.userid : null;
+	$: filteredUsers = users.filter(user => {
+		const fullName = `${user.nombre} ${user.apellido}`.toLowerCase();
+		return fullName.includes(searchQuery.toLowerCase());
 	});
 
 	function handleClose() {
 		showModal = false;
 	}
-
-	function loadBadges() {
-		axios
-			.get(`https://luma-server.onrender.com/api/badge-obtained/user-client?userId=${userId}`)
-			.then((response) => {
-				badges = response.data.badges;
-				console.log('Badges:', badges);
-			})
-			.catch((error) => {
-				console.error('Error fetching badges:', error);
-			});
-	}
-
-	function getUnlockedBadges(badges) {
-		return badges.filter((badge) => badge.unlocked);
-	}
-
-	onMount(() => {
-		loadBadges();
-	});
-
-	const sortedResponse = getUnlockedBadges(badges);
-
-	$: getCurrentPageItems(currentPage);
 
 	function getCurrentPageItems(currentPage) {
 		const start = (currentPage - 1) * itemsPerPage;
@@ -70,51 +51,48 @@
 		}
 	}
 
-	async function getUserInfo() {
-		await axios
-			.get(`https://luma-server.onrender.com/api/user/${$page.params.userid}`)
+	async function getUserInfo(){
+		await axios.get(`https://luma-server.onrender.com/api/user/${$page.params.userid}`)
 			.then((response) => {
 				console.log(response.data);
-				userData = response.data[0];
+				userData = response.data[0]
 			})
 			.catch((error) => {
 				console.log(response.data);
-			});
+			})
 	}
 
-	async function getAllUsers() {
-		await axios
-			.get('https://luma-server.onrender.com/api/user/')
+	async function getAllUsers(){
+		await axios.get('https://luma-server.onrender.com/api/user/')
 			.then((response) => {
 				console.log(response.data);
-				users = response.data || [];
+				users = response.data || []
 			})
 			.catch((error) => {
 				console.log(error);
-			});
+			})
 	}
 
-	async function getUserBadges() {
-		await axios
-			.get(`https://luma-server.onrender.com/api/badge-obtained/user?userId=${$page.params.userid}`)
+	async function getUserBadges(){
+		await axios.get(`https://luma-server.onrender.com/api/badge-obtained/user?userId=${$page.params.userid}`)
 			.then((response) => {
 				console.log(response.data);
-				loading = false;
-				userBadges = response.data || [];
+				loading = false
+				userBadges = response.data || []
 			})
 			.catch((error) => {
 				console.log(error.data);
-				loading = false;
-			});
+				loading = false
+			})
 	}
 
 	//TODO: dura un poco al momento de refrescar la pagina, validar que es el tema que esta ocasionando
 	//TODO: revisar que alternativa existe para mejorar la experiencia
 	function handleNavigation(userId) {
 		goto(`/user/${userId}`).then(async () => {
-			await getAllUsers();
-			await getUserBadges();
-			await getUserInfo();
+			await getAllUsers()
+			await getUserBadges()
+			await getUserInfo()
 		});
 	}
 </script>
@@ -122,25 +100,22 @@
 <div class="main">
 	<div class="side-bar">
 		<div class="search-container">
-			<!--			<p>Buscar perfiles</p>-->
-			<input type="text" placeholder="Buscar perfiles" class="search-bar" />
+			<input type="text" placeholder="Buscar perfiles" class="search-bar" bind:value={searchQuery}>
 		</div>
 
 		<div class="profiles-container">
-			{#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] as profile}
-				<div
-					class="profile {selectedProfile === profile ? 'selected' : ''}"
-					on:click={() => {
-						selectedProfile = profile;
-					}}
-				>
-					<p>Profile {profile}</p>
-					+ny6
-				</div>
-			{/each}
+			{#if filteredUsers.length > 0}
+				{#each filteredUsers as user}
+					<div class="profile {currentUserID === user.Usuario_ID ? 'selected' : ''}" on:click={() => {handleNavigation(user.Usuario_ID)}}>
+						<p>{user.nombre} {user.apellido}</p>
+					</div>
+				{/each}
+			{:else}
+				<p>No users found.</p>
+			{/if}
 		</div>
 	</div>
-
+	
 	<div class="content">
 		<div class="profile-info">
 			<div class="top"></div>
@@ -149,17 +124,18 @@
 				<div class="user-profile-picture">
 					<div class="avatar placeholder">
 						<div class="bg-neutral text-neutral-content w-24 rounded-full">
-							<span class="text-3xl"> A </span>
+						<!--TODO: pending user profile-->
+						<span class="text-3xl">
+							{userData.nombre?.[0]}{userData.apellido?.[0]}
+						</span>
 						</div>
 					</div>
 				</div>
 
 				<div class="user-details">
-					{#if loading}
-						<!-- Estado de carga -->
+					{#if loading} <!-- Estado de carga -->
 						<p>Loading user data...</p>
-					{:else}
-						<!-- Datos cargados -->
+					{:else} <!-- Datos cargados -->
 						<p class="name">{userData.nombre} {userData.apellido}</p>
 						<p class="mail">{userData.correo}</p>
 					{/if}
@@ -170,129 +146,116 @@
 		<p class="badge-title">Insignias</p>
 		<div class="scrollable">
 			<div class="badges-pagination">
-				{#each getCurrentPageItems(currentPage) as badge}
-					<div
-						class="badge {badge.unlocked ? '' : 'locked'}"
-						on:click={() => {
-							if (badge.unlocked) {
-								showModal = true;
-								selectedBadge = badge;
-							}
-						}}
-					>
-						{badge.title}
-
-						{#if !badge.unlocked}
-							<div class="lock-icon">
-								<LockKeyhole size={140} />
+				{#if loading} <!--Loading state-->
+					<p>Loading...</p>
+				{:else} <!--Loading state-->
+					{#if userBadges.length > 0} <!-- Check for badges -->
+						{#each getCurrentPageItems(currentPage) as badge}
+							<div class="badge" on:click={() => { showModal = true; selectedBadge = badge }}>
+								{badge.Insignias.nombre}
 							</div>
-						{/if}
-					</div>
-				{/each}
-			</div>
+						{/each}
+					{:else} <!-- No results (badges) -->
+						<p>No badges available.</p>
+					{/if}
+				{/if} <!--End of Loading state-->
 
-			<!--{#if sortedResponse.length > 10}-->
-			<div class="pagination-controls">
-				<button on:click={prevPage} disabled={currentPage === 1}>Previous</button>
-				<span>Page {currentPage} of {Math.ceil(sortedResponse.length / itemsPerPage)}</span>
-				<button
-					on:click={nextPage}
-					disabled={currentPage === Math.ceil(sortedResponse.length / itemsPerPage)}>Next</button
-				>
+				</div>
+
+					{#if userBadges.length > 0}
+						<div class="pagination-controls">
+							<button on:click={prevPage} disabled={currentPage === 1}>Previous</button>
+							<span>Page {currentPage} of {Math.ceil(userBadges.length / itemsPerPage)}</span>
+							<button on:click={nextPage} disabled={currentPage === Math.ceil(userBadges.length / itemsPerPage)}>Next</button>
+						</div>
+					{/if}
+
 			</div>
-			<!--{/if}-->
 		</div>
-	</div>
 </div>
 
-<BadgeDetailModal show={showModal} on:close={handleClose} badgeData={selectedBadge} />
+<BadgeDetailModal show={showModal} on:close={handleClose} badgeData={selectedBadge}/>
 
 <style>
-	.main {
-		background-color: var(--luma-color-gray-50);
-		width: 100vw;
-		height: 90vh;
-		display: flex;
+	.main{
+			background-color: var(--luma-color-gray-50);
+      width: 100vw;
+			height: 90vh;
+			display: flex;
 	}
 
-	.side-bar {
-		width: 270px;
-		height: 90vh;
-		background-color: var(--luma-color-gray-100);
-		border-right: 3px solid var(--luma-color-gray-200);
-		padding: 1.5rem 1rem 1rem 1rem;
-		display: flex;
-		flex-direction: column;
+	.side-bar{
+      width: 270px;
+      height: 90vh;
+      background-color: var(--luma-color-gray-100);
+      border-right: 3px solid var(--luma-color-gray-200);
+			padding: 1.5rem 1rem 1rem 1rem;
+			display: flex;
+			flex-direction: column;
 	}
 
-	.search-container {
-		display: flex;
-		flex-direction: column;
-		gap: var(--luma-half-element-spacing);
+	.search-container{
+      display: flex;
+			flex-direction: column;
+			gap: var(--luma-half-element-spacing);
 	}
 
-	/*.search-container p{*/
-	/*		font-size: var(--luma-h5-font-size);*/
-	/*}*/
-
-	.search-container .search-bar {
-		padding: var(--luma-half-element-spacing);
-		border-radius: 8px;
+  .search-container .search-bar{
+			padding: var(--luma-half-element-spacing);
+			border-radius: 8px;
 	}
 
-	.profiles-container {
-		margin-top: 1rem;
-		display: flex;
-		flex-direction: column;
-		gap: var(--luma-half-element-spacing);
-		overflow: auto;
-		/*-ms-overflow-style: none;  !* IE and Edge *!*/
-		/*scrollbar-width: none;  !* Firefox *!*/
-	}
+	.profiles-container{
+			margin-top: 1rem;
+			display: flex;
+			flex-direction: column;
+			gap: var(--luma-half-element-spacing);
+			overflow: auto;
+  }
 
-	.profile {
+	.profile{
 		padding-block: var(--luma-half-element-spacing);
 		padding-inline: 1rem;
 	}
 
-	.profile:hover {
-		color: var(--luma-color-orange-600);
-		font-weight: bold;
+	.profile:hover{
+      color: var(--luma-color-orange-600);
+      font-weight: bold;
 	}
 
-	.profile.selected {
-		border-left: 3px solid var(--luma-color-orange-600);
-		color: var(--luma-color-orange-600);
+	.profile.selected{
+      border-left: 3px solid var(--luma-color-orange-600);
+      color: var(--luma-color-orange-600);
 	}
 
-	.content {
-		width: 90vw;
-		padding-top: 1.5rem;
-		padding-inline: 4rem;
+	.content{
+			width: 90vw;
+			padding-top: 1.5rem;
+			padding-inline: 4rem;
 	}
 
-	.profile-info {
-		border-bottom: 3px solid var(--luma-color-gray-200);
-		margin-bottom: 1.5rem;
+	.profile-info{
+			border-bottom: 3px solid var(--luma-color-gray-200);
+			margin-bottom: 1.5rem;
 	}
 
-	.top {
-		width: 100%;
-		height: 12vh;
-		background-color: var(--luma-color-secondary-orange);
-		opacity: 0.8;
-		border-radius: 8px;
+	.top{
+			width: 100%;
+			height: 12vh;
+			background-color: var(--luma-color-secondary-orange);
+      opacity: 0.8;
+			border-radius: 8px;
 	}
 
-	.bottom {
-		padding-inline: var(--luma-element-spacing);
-		/*background-color: lightgrey;*/
-		display: flex;
-		gap: 1rem;
-		height: 15vh;
+	.bottom{
+			padding-inline: var(--luma-element-spacing);
+			/*background-color: lightgrey;*/
+      display: flex;
+      gap: 1rem;
+			height: 15vh;
 	}
 
-	.profile-info .user-profile-picture {
+	.profile-info .user-profile-picture{
 		background-color: white;
 		height: 110px;
 		padding: var(--luma-half-element-spacing);
@@ -300,89 +263,90 @@
 		transform: translateY(-40%);
 	}
 
-	.profile-info .user-details {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		transform: translateY(-15%);
+	.profile-info .user-details{
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+      transform: translateY(-15%);
 	}
 
-	.profile-info .user-details .name {
-		font-size: var(--luma-title-font-size);
-		font-weight: bold;
+  .profile-info .user-details .name{
+			font-size: var(--luma-title-font-size);
+			font-weight: bold;
+  }
+
+  .profile-info .user-details .mail{
+  }
+
+	.badge-title{
+			font-size: var(--luma-h4-font-size);
 	}
 
-	.profile-info .user-details .mail {
-	}
+  .scrollable{
+			/*background-color: pink;*/
+      height: 43vh;
+      overflow: auto;
+			padding-top: var(--luma-half-element-spacing);
+  }
 
-	.badge-title {
-		font-size: var(--luma-h4-font-size);
-	}
+  .badges-pagination {
+      padding: var(--luma-half-element-spacing);
+      display: grid;
+      grid-template-columns: repeat(5, 1fr);
+      grid-auto-rows: 100px;
+      grid-gap: 20px;
+      height: 236px;
+  }
 
-	.scrollable {
-		/*background-color: pink;*/
-		height: 43vh;
-		overflow: auto;
-		padding-top: var(--luma-half-element-spacing);
-	}
+  .pagination-controls {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-top: 1rem;
+  }
 
-	.badges-pagination {
-		padding: var(--luma-half-element-spacing);
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		grid-auto-rows: 100px;
-		grid-gap: 20px;
-		height: 236px;
-	}
+  .pagination-controls button {
+      background-color: var(--luma-color-secondary-orange);
+      color: white;
+      padding: 0.5rem 1rem;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+  }
 
-	.pagination-controls {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		margin-top: 1rem;
-	}
+  .pagination-controls button:disabled {
+      background-color: var(--luma-color-gray-300);
+      cursor: not-allowed;
+  }
 
-	.pagination-controls button {
-		background-color: var(--luma-color-secondary-orange);
-		color: white;
-		padding: 0.5rem 1rem;
-		border: none;
-		border-radius: 4px;
-		cursor: pointer;
-	}
+  .badge{
+      background-color: var(--luma-color-gray-50);
+      width: auto;
+      height: 100px;
+      border: 0px;
+      border-radius: 8px;
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 2px 5px;
+      position: relative;
+  }
 
-	.pagination-controls button:disabled {
-		background-color: var(--luma-color-gray-300);
-		cursor: not-allowed;
-	}
+  .locked::after{
+      content: '';
+      background-color: var(--luma-color-gray-500);
+      width: 100%;
+      height: 100%;
+      border-radius: 8px;
+      position: absolute;
+      top: 0;
+      left: 0;
+  }
 
-	.badge {
-		background-color: var(--luma-color-gray-50);
-		width: auto;
-		height: 100px;
-		border: 0px;
-		border-radius: 8px;
-		box-shadow: rgba(0, 0, 0, 0.24) 0px 2px 5px;
-		position: relative;
-	}
+  .locked .lock-icon {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      color: white;
+      z-index: 2;
+  }
 
-	.locked::after {
-		content: '';
-		background-color: var(--luma-color-gray-500);
-		width: 100%;
-		height: 100%;
-		border-radius: 8px;
-		position: absolute;
-		top: 0;
-		left: 0;
-	}
-
-	.locked .lock-icon {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		position: absolute;
-		color: white;
-		z-index: 2;
-	}
 </style>
