aa57b0f3caf7c23eaebdb6d5d17ca70f6e12220d
huanhaowu <1104326@est.intec.edu.do>
2024-11-28 08:17:28 -0400

    feat: team config loading integration

--- src/routes/(app)/(project)/[id]/config/team/+page.svelte
+++ src/routes/(app)/(project)/[id]/config/team/+page.svelte
@@ -10,11 +10,19 @@
 	console.log($projectData);
 
 	onMount(async () => {
-		await getAllRoles()
-		await getAllMembers()
-		loading = false
+		loading = true
+		try {
+			await Promise.all([
+				getAllRoles(),
+				getAllMembers()
+			])
+		} catch (error) {
+			console.error("Error fulfilling promises:", error);
+		} finally {
+			loading = false
+		}
 		selectedRoles = projectMembers.map((row) => row.Rol_ID);
-		console.log(selectedRoles);
+		// console.log(selectedRoles);
 	})
 
 	let showInvitationModal = false
@@ -65,6 +73,12 @@
 
 	let columns = ['Miembro', 'Correo', 'Rol', '']
 </script>
+
+{#if loading}
+	<div class="overlay">
+		<span class="loader"></span>
+	</div>
+{/if}
 
 <p class="title">Configuración de equipo</p>
 
@@ -188,5 +202,49 @@
 
     table tr th.action{
         width: 80px;
+    }
+
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        font-size: 1.5rem;
+        z-index: 1000;
+    }
+
+    .loader {
+        width: 48px;
+        height: 48px;
+        border-radius: 50%;
+        position: relative;
+        animation: rotate 1s linear infinite
+    }
+    .loader::before {
+        content: "";
+        box-sizing: border-box;
+        position: absolute;
+        inset: 0px;
+        border-radius: 50%;
+        border: 5px solid #FFF;
+        animation: prixClipFix 2s linear infinite ;
+    }
+
+    @keyframes rotate {
+        100%   {transform: rotate(360deg)}
+    }
+
+    @keyframes prixClipFix {
+        0%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+        25%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 0,100% 0,100% 0)}
+        50%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,100% 100%,100% 100%)}
+        75%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 100%)}
+        100% {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 0)}
     }
 </style>
