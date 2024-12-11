f4b5d3233536d9bb5456fec2b55450b7ab348b30
GleidyEspinal <1104225@est.intec.edu.do>
2024-12-10 22:19:08 -0400

    feat: completing editing a project feature

--- src/routes/(app)/(project)/[id]/config/+page.svelte
+++ src/routes/(app)/(project)/[id]/config/+page.svelte
@@ -1,11 +1,118 @@
 <script>
+	import axios from 'axios';
 	import { projectData } from '$lib/stores/projectStore';
+	import { showToast } from '$lib/stores/toastStore';
 
 	console.log($projectData);
+
+	let projectName = $projectData.nombre;
+	let projectDescription = $projectData.descripcion;
+	let projectId = $projectData.Proyecto_ID;
+	let invalidInput;
+
+	let userData = JSON.parse(localStorage.getItem('sb-kyttbsnmnrayejpbxmpp-auth-token'));
+	let userID = userData.user.id;
+
+	function validate() {
+		invalidInput = false;
+		if (projectName.length > 50) {
+			console.error('El nombre del proyecto no debe exceder 50 caracteres.');
+			invalidInput = true;
+			return;
+		}
+
+		axios
+			.put('https://luma-server.onrender.com/api/projects', {
+				nombre: projectName,
+				descripcion: projectDescription,
+				projectId: projectId,
+				requestUserId: userID
+			})
+			.then((response) => {
+				console.log('Proyecto editado con ID:', projectId);
+				console.log('Proyecto editado:', response.data);
+
+				showToast(`Informacion actualizada`, {
+					type: 'info',
+					duration: 5000,
+					theme: 'dark'
+				});
+			})
+			.catch((error) => {
+				console.error('Error al editar el proyecto:', error);
+
+				showToast(`No tienes permiso para editar`, {
+					type: 'error',
+					duration: 5000,
+					theme: 'dark'
+				});
+			});
+	}
+	function cancelChanges() {
+		// Resetear los valores
+		projectName = $projectData.nombre;
+		projectDescription = $projectData.descripcion;
+	}
 </script>
 
-Hola soy la pagina de configuracion
+<div class="content">
+	<p class="title">Nuevo Proyecto</p>
+
+	<div class="container">
+		<label for="projectName">
+			<span>Nombre del Proyecto</span>
+			<input id="projectName" type="text" required bind:value={projectName} />
+		</label>
+
+		<label for="projectDescription">
+			<span>Descripción</span>
+			<textarea id="projectDescription" required bind:value={projectDescription}></textarea>
+		</label>
+
+		<div class="controls">
+			<button class="btn btn-outline" on:click={cancelChanges}> Cancelar </button>
+			<button type="submit" class="btn btn-primary" on:click={validate}> Guardar </button>
+		</div>
+	</div>
+</div>
 
 <style>
+	.content {
+		display: flex;
+		flex-direction: column;
+	}
 
+	.content .title {
+		font-size: var(--luma-h4-font-size);
+	}
+
+	.container {
+		width: 70%; /* Mantener el ancho amplio */
+		padding-top: 1rem;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	label {
+		display: flex;
+		flex-direction: column;
+	}
+
+	input,
+	textarea {
+		margin-top: var(--luma-half-element-spacing);
+		border-radius: 8px;
+		padding: var(--luma-half-element-spacing);
+		width: 100%; /* Asegura que los campos ocupen todo el ancho del contenedor */
+	}
+
+	textarea {
+		resize: vertical;
+		min-height: 120px; /* Aumenté la altura mínima del textarea */
+	}
+
+	.controls {
+		text-align: right;
+	}
 </style>
