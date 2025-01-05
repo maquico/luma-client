c99a0e1d84202f922f672b3b95a76918ebcec5e6
maquico <1104666@est.intec.edu.do>
2025-01-05 11:47:51 -0400

    fix(invite): toasts added

--- src/components/modals/invite.modal.svelte
+++ src/components/modals/invite.modal.svelte
@@ -1,35 +1,46 @@
 <script>
-	import Modal from '$components/modal.svelte'
-	import {createEventDispatcher} from 'svelte';
+	import Modal from '$components/modal.svelte';
+	import { createEventDispatcher } from 'svelte';
 	import axios from 'axios';
 	import { projectData } from '$lib/stores/projectStore';
+	import { showToast } from '$lib/stores/toastStore.js';
 
-	let invitationMail
-
-	console.log($projectData);
-
+	let invitationMail;
 	const dispatch = createEventDispatcher();
-
 	export let show = true;
 
 	const close = () => {
 		show = false;
-		dispatch('close')
+		dispatch('close');
+	};
+
+	// Email validation function
+	function isValidEmail(email) {
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		return emailRegex.test(email);
 	}
 
-	//TODO: add a toast when invitation has been send
+	// Send invitation with validation and error handling
 	async function sendInvitation() {
-		await axios.post('https://luma-server.onrender.com/api/invitation/send/', {
-			email: invitationMail,
-			projectId: $projectData.Proyecto_ID
-		})
+		if (!isValidEmail(invitationMail)) {
+			showToast('error', 'Formato de correo inválido', 'Por favor, ingrese un correo electrónico válido.');
+			return;
+		}
+
+		await axios
+			.post('https://luma-server.onrender.com/api/invitation/send/', {
+				email: invitationMail,
+				projectId: $projectData.Proyecto_ID,
+			})
 			.then((response) => {
 				console.log(response.data);
-				dispatch('close')
+				dispatch('close');
+				showToast('Invitación enviada exitosamente', { type: 'success', duration: 5000 });
 			})
 			.catch((error) => {
-				console.log(error.data);
-			})
+				console.error(error);
+				showToast('Error al enviar la invitación', { type: 'error', duration: 5000 });
+			});
 	}
 </script>
 
@@ -45,7 +56,12 @@
 				electrónico
 			</p>
 			<label class="form-control w-full">
-				<input type="email" placeholder="Type here" class="input input-bordered w-full" bind:value={invitationMail}/>
+				<input
+					type="email"
+					placeholder="Type here"
+					class="input input-bordered w-full"
+					bind:value={invitationMail}
+				/>
 			</label>
 			<div class="controls">
 				<button class="btn btn-outline" on:click={close}>
@@ -60,14 +76,14 @@
 {/if}
 
 <style>
-    .controls{
-        text-align: right;
-    }
+	.controls {
+		text-align: right;
+	}
 
-    form{
-        margin-top: 1rem;
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-    }
+	form {
+		margin-top: 1rem;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
 </style>
